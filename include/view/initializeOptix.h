b1b549be593b1bc7ea5784df25756e866c615633
foobar <foo@bar>
2021-11-01 21:34:05 +0100

    add sutil and support directory to config, start with initialization as in optixTriangle example

---
+++ include/view/initializeOptix.h
@@ -0,0 +1,10 @@
+//
+// Created by ich on 11/1/21.
+//
+
+#ifndef TROLLSAR_INITIALIZEOPTIX_H
+#define TROLLSAR_INITIALIZEOPTIX_H
+
+void initializeOptix();
+
+#endif //TROLLSAR_INITIALIZEOPTIX_H
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ich on 11/1/21.
+//
+
+#include <view/initializeOptix.h>
+
+int main(int iargc, char *argv[]) {
+    initializeOptix();
+}
